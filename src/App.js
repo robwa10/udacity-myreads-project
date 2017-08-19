dbf31d8899023a3a382d431205a9e3b81e42d51f
Robert Hubbard <rob.c.hubbard@gmail.com>
2017-08-19 14:56:40 -0500

    Make shelf state the same on search page

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ class App extends Component {
   componentDidMount() {
     BooksAPI.getAll().then((books) => {
       this.setState({ books });
+      console.log(this.state.books)
     });
   }
 
@@ -39,8 +40,14 @@ class App extends Component {
   updateQuery = (query) => {
     this.setState({ query: query.trim() })
     if (this.state.query) {
-      BooksAPI.search(this.state.query, 20).then((books) => {
-        this.setState({ searchBooks: books});
+      BooksAPI.search(this.state.query, 20).then((newBooks) => {
+        if (newBooks.length) {
+          newBooks.forEach((book) => {
+            let thisBook = this.state.books.find((bar) => bar.title === book.title);
+            book.shelf = thisBook ? thisBook.shelf : 'none';
+          });
+        }
+        this.setState({ searchBooks: newBooks});
         this.state.searchBooks.sort(sortBy(this.state.searchBooks.title));
       });
     }
